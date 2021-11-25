f0d967dfa0c0bb5f9cff02ee076a85c918366349
rayyanInqline786 <69470014+rayyanInqline786@users.noreply.github.com>
2021-11-25 21:16:03 +0500

    update code

--- testproject/urls.py
+++ testproject/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('',views.index, name="index"),
     path('home/',views.home, name="home"),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
